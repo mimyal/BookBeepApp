58aa162467c824813a3b1e5afa854820786b2ffa
Alma Maizels <aymaiz@yahoo.com>
2016-12-31 12:02:08 -0800

    First commit

---
+++ app/src/main/java/io/dreamsphere/bookbeep/MainActivity.java
@@ -0,0 +1,13 @@
+package io.dreamsphere.bookbeep;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // runs the activity_main.xml file
+    }
+}
